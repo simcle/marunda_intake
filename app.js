250bf83050a8fe45b90dcf1786bbae9f177eeb0b
NendiSofiandy <nendisofiandy80@gmail.com>
2025-06-08 14:02:07 +0700

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import startPooling from "./flow.js";
 import eventBus from "./event.js";
 
 
-eventBus.on('flowmeter', (data) => {
+eventBus.on('flowrate', (data) => {
     console.log('ini data darai event flow', data)
 })
 
