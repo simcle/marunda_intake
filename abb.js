da71a9bf50cb6e8a0ae352da4568b5e1e99e7164
NendiSofiandy <nendisofiandy80@gmail.com>
2025-06-12 16:06:31 +0700

    update

--- abb.js
+++ abb.js
@@ -4,14 +4,18 @@ const client = new ModbusRTU()
 
 
 const connectClient = async () => {
-    await client.connectRTUBuffered('/dev/ttyS2', {baudRate: 9600})
+    await client.connectRTUBuffered('/dev/ttyS2', {baudRate: 19200})
     client.setID(1)
 
     setInterval(async () => {
         console.log('send request')
-        const data = await client.readHoldingRegisters(0, 10)
-        console.log('response')
-        console.log(data) 
+        try {
+            const data = await client.readHoldingRegisters(0, 10)
+            console.log('response')
+            console.log(data) 
+        } catch (error) {
+            console.log(error.message)
+        }
     }, 1000)
 }
 
