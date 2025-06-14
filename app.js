6d515168859f8e424aaf1920e90f0d610cb8cf19
NendiSofiandy <nendisofiandy80@gmail.com>
2025-06-14 09:44:15 +0700

    update

--- app.js
+++ app.js
@@ -2,11 +2,41 @@ import startPoolingflow from "./flow.js";
 import startPoolingPump from "./pump.js";
 import eventBus from "./event.js";
 
+import mqtt from "mqtt";
+let mqttClient = null
+let mqttIsConnected = false
+mqttClient = mqttClient = mqtt.connect('mqtt://mqtt.ndpteknologi.com', {
+    clientId: 'marunda_ipa' + Math.random().toString(16).slice(2, 8),
+    reconnectPeriod: 3000
+})
 
+mqttClient.on('connect', () => {
+    console.log('MQTT is connected')
+    mqttIsConnected = true
+})
+
+mqttClient.on('close', () => {
+    mqttIsConnected = false
+})
+
+mqttClient.on('error', () => {
+    mqttIsConnected = false
+})
+
+const data = {
+    flowrate: 0,
+    pmp1: 0,
+    pmp2: 1
+}
 
 // flowrate
 eventBus.on('flowrate', (data) => {
     console.log('ini data darai event flow', data)
+    data.flowrate = data
+})
+
+setInterval(() => {
+    mqttClient.publish('marunda/intake', JSON.stringify(data))
 })
 
 startPoolingPump()
