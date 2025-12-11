5b548f6647b27a8f5508f7c6229479574479a776
NendiSofiandy <nendisofiandy80@gmail.com>
2025-12-11 15:07:43 +0700

    update

--- abb.js
+++ abb.js
@@ -28,6 +28,11 @@ async function readRegisters() {
         const res = await client.readHoldingRegisters(265, 2);  
         // mulai dari reg 0, length 4
         console.log("Data:", res.data);
+        const buf = Buffer.alloc(4);
+        buf.writeUInt16BE(res.data[0], 0);  // high word
+        buf.writeUInt16BE(res.data[1], 2);  // low word
+        const motorTorque = buf.readFloatBE(0); // atau LE jika format-nya little endian
+        console.log('Motor Torque (%) =', motorTorque);
 
     } catch (err) {
         console.error("❌ Read error:", err.message);
