5f8a78ec2bc476a238b199f988a036a88c9da917
NendiSofiandy <nendisofiandy80@gmail.com>
2025-12-11 16:15:53 +0700

    update

--- acs550.js
+++ acs550.js
@@ -7,7 +7,7 @@ const client = new ModbusRTU();
 async function connect() {
     try {
         await client.connectRTUBuffered("/dev/ttyACM0", {
-            baudRate: 9600,
+            baudRate: 19200,
             dataBits: 8,
             stopBits: 1,
             parity: "none",
