2cdaf124ac97d16e5d29d26fa41061996cd9b53a
NendiSofiandy <nendisofiandy80@gmail.com>
2025-12-11 13:11:17 +0700

    update

--- abb.js
+++ abb.js
@@ -6,7 +6,7 @@ const client = new ModbusRTU();
 
 async function connect() {
     try {
-        await client.connectRTUBuffered("/dev/ttyS0", {
+        await client.connectRTUBuffered("/dev/ttyS2", {
             baudRate: 9600,
             dataBits: 8,
             stopBits: 1,
