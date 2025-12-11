1dd74d42344806a6a673a2af3706cc93dc6721c9
NendiSofiandy <nendisofiandy80@gmail.com>
2025-12-11 23:34:54 +0700

    update

--- abb.js
+++ abb.js
@@ -26,7 +26,7 @@ async function connect() {
 async function readRegisters() {
     try {
         // const res = await client.readHoldingRegisters(0, 9);  
-        const res = await client.readHoldingRegisters(20211, 2)
+        const res = await client.readHoldingRegisters(20213, 2)
         // mulai dari reg 0, length 4
         console.log("Data:", res.buffer.swap16().readInt32LE(0));
 
