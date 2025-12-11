75a3620b89377a01a452bda6b22c0fdf792a3315
NendiSofiandy <nendisofiandy80@gmail.com>
2025-12-11 12:07:30 +0700

    update abb.js

--- abb.js
+++ abb.js
@@ -3,21 +3,27 @@ import ModbusRTU from "modbus-serial";
 console.log('hallo')
 const client = new ModbusRTU()
 
+const connect = async () => {
+    try {
+        await client.connectRTUBuffered('/dev/ttyS2', {baudRate: 19200})
+        client.setID(1)
+        client.setTimeout(1000)
+        
+    } catch (error) {
+        console.log('error connection', error)
+    }
+}
 
-const connectClient = async () => {
-    await client.connectRTUBuffered('/dev/ttyS2', {baudRate: 19200})
-    client.setID(1)
+const readRegisters = async () => {
+    const res = await client.readHoldingRegisters(1, 4)
+    console.log(res)
+}
 
+const run = async () => {
+    await connect()
     setInterval(async () => {
-        console.log('send request')
-        try {
-            const data = await client.readHoldingRegisters(1, 2)
-            console.log('response')
-            console.log(data) 
-        } catch (error) {
-            console.log(error.message)
-        }
+        await readRegisters()
     }, 1000)
 }
 
-connectClient()
+run()
