73a7eb8e975ef87551f1378ab4e09f2bc75dc17c
NendiSofiandy <nendisofiandy80@gmail.com>
2026-01-09 11:07:06 +0700

    update

--- flow.js
+++ flow.js
@@ -36,8 +36,10 @@ const pollData = async () => {
         const data = await client.readHoldingRegisters(0, 1);
         const val = data.data
         const val1 = data.buffer.readInt16BE()
+        const val2 = data.buffer.readInt16LE()
         console.log('buff', data.buffer)
-        console.log('ma', val1)
+        console.log('BE', val1)
+        console.log('LE', val2)
         const currMa = adcToMilliamp(val)
         // console.log('ma', currMa)
         const flow = milliampToFlow(currMa)
--- tes.js
+++ tes.js
@@ -1,3 +1,4 @@
-const data = [31213]
+const data = Buffer.from([0x7a, 0x33]);
 
-
+const val = data.readInt16BE()
+console.log('Buffer:', val);
