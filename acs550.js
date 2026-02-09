405cd4043f1550c98e9a8a9e90b19f1fdfbacb0d
NendiSofiandy <nendisofiandy80@gmail.com>
2026-02-09 18:21:02 +0700

    update

--- acs550.js
+++ acs550.js
@@ -12,7 +12,7 @@ client.on("debug", msg => {
 async function connect() {
     try {
         await client.connectRTUBuffered("/dev/ttyS2", {
-            baudRate: 9600,
+            baudRate: 19200,
             dataBits: 8,
             stopBits: 1,
             parity: "none",
@@ -37,7 +37,7 @@ async function connect() {
 async function readRegisters() {
     try {
         // Baca 4 register: status, speed, current, DC voltage
-        const res = await client.readHoldingRegisters(4, 1);
+        const res = await client.readHoldingRegisters(0, 1);
         console.log(res)
     } catch (err) {
         console.error("❌ Read error:", err.message);
